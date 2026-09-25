1c8030805598f6666feeb419f1886a3954a8a86a
cacaoradio <cacaoradiostation@gmail.com>
2026-09-25 20:57:28 +0200

    Update service worker cache version and event handling
    Updated service worker to use a new cache version and improved the activate event handling to delete old caches.

--- sw.js
+++ sw.js
@@ -1,9 +1,27 @@
-const CACHE='cacao-radio-v1';
+const CACHE='cacao-radio-v2';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+
+self.addEventListener('install',e=>{
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(c=>c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(
+        keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+      ))
+      .then(()=>self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch',e=>{
   if(e.request.mode==='navigate'){
-    e.respondWith(fetch(e.request).catch(()=>caches.match('./index.html')));
+    e.respondWith(
+      fetch(e.request).catch(()=>caches.match('./index.html'))
+    );
   }
 });
