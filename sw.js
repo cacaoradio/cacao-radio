4e029c7ca3ecd22b9e52f3361c053b901f0a6520
cacaoradio <cacaoradiostation@gmail.com>
2026-09-25 17:25:08 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE='cacao-radio-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  if(e.request.mode==='navigate'){
+    e.respondWith(fetch(e.request).catch(()=>caches.match('./index.html')));
+  }
+});
